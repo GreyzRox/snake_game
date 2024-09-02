cf5a071fed2f5ea74c5919254a4b1a76bebb63d2
GreyzRox <guillaume.dupart.pro@gmail.com>
2024-09-02 18:34:13 +0200

    changement mouvement & camera (a debogger)

--- snake.py
+++ snake.py
@@ -9,26 +9,36 @@ ground = create_ground()
 wall = create_wall()
 player = init_player()
 camera.position = (player.x,player.y+10,player.z)
-camera.fov = 100
-camera.rotation_x = 20
+camera.fov = 120
+camera.rotation_x = 15
 direction = Vec3(0, 0, 0)
-player_speed = 5   
+player_speed = 10
 Sky()
 
 def update():
     global direction
     
-    if held_keys['z']:
+    if held_keys['up arrow']:
         direction = Vec3(0, 0, 1)
-    elif held_keys['s']:
+        player.rotation_y = min(player.rotation_y + player_speed * time.dt, 0)
+        camera.rotation_y = min(camera.rotation_y + player_speed * time.dt, 0)
+    elif held_keys['down arrow']:
         direction = Vec3(0, 0, -1)
-    elif held_keys['q']:
+        player.rotation_y = max(player.rotation_y - player_speed * time.dt, 180)
+        camera.rotation_y = max(camera.rotation_y - player_speed * time.dt, 180)
+    elif held_keys['left arrow']:
         direction = Vec3(-1, 0, 0)
-    elif held_keys['d']:
+        player.rotation_y = max(player.rotation_y - player_speed * time.dt, -90)
+        camera.rotation_y = max(camera.rotation_y - player_speed * time.dt, -90)
+    elif held_keys['right arrow']:
         direction = Vec3(1, 0, 0)
+        player.rotation_y = min(player.rotation_y + player_speed * time.dt, 90)
+        camera.rotation_y = min(camera.rotation_y + player_speed * time.dt, 90)
+
     
     player.position += direction * time.dt * player_speed
     
-    camera.position = (player.x, player.y + 20, player.z - 40)
+    camera.position = (player.x, player.y + 7, player.z+2)
+    print(player.rotation_y)
 
 app.run()
